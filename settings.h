a1943ce44a4194dbb0e82cf2dcea4afae5819727
CTVarlan <ctvarlan@users.noreply.github.com>
2020-07-02 19:42:11 -0400

    Update settings.h

--- settings.h
+++ settings.h
@@ -9,14 +9,6 @@ unsigned 	long 	ChannelNumber 	= XXXXXX;		//to read from and write to
 const 		char* 	myWriteAPIKey 	= "xxxxxxxxxxxxxxxx"; 	//16 characters
 const		char* 	myReadAPIKey	= "xxxxxxxxxxxxxxxx"; 	//16 characters
 
-//------PushBullet settings-----------------------------------------------------
-const 		char* 	host 				= "api.pushbullet.com";
-const 		int 	httpsPort 	= 443; 			// the required port
-const 		char* 	accessToken 		= "o.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";// 34 characters starting with 'o.'
-			//get it from the pushbullet account
-const 		char* 	fingerprint 		= "XX XX XX ... XX";//
-			//get it from the browser
 
 // Hardware information.
 #define SLEEP_TIME_SECONDS 	1800		// 
-#define TWITTER_ACC		"your Twitter handler"  //@whatever
