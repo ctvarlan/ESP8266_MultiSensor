96e8b821760919bb3cd0b54a04f01302ad68bde0
CTVarlan <ctvarlan@users.noreply.github.com>
2020-07-02 20:28:43 -0400

    Update MultiSensor.ino
    Some update for the code

--- MultiSensor.ino
+++ MultiSensor.ino
@@ -1,64 +1,288 @@
-/* MultiSensor_2_3.ino
+/* MultiSensor_2_5.ino
 
-ESP8266 uses 4 of its GPIOs and the only AnalogIn to drive 4 sensors.
-
-What is needed:
-[_]	WiFi access:
-	[_] ssid		The name of your Access Point (a.k.a. the router)
-	[_] pass		The password to access the router
-
-[_]	ThingSpeak account [free version]
-	[_]	the Channel Number
-	[_]	the WriteAPIKey
-[_]	Tweeter account [free]
-	[_]	the handler (@...)
-	[_]	the APIKey 
-	[_]	the settings done within ThingSpeak
-[_]	PushBullet account [free version]
-	[_]	the Access Tokens from your account
-	[_] the fingerprint from the browser
-
-All these should be handled through a separate file, here is named "settings.h"
-
+--------------------------------------------------------------------------------
 */
 
-#include "ThingSpeak.h"
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+//#include <WiFiClientSecure.h>
+#include <esp8266-google-home-notifier.h>		//ghn
+#include "ThingSpeak.h"
 #include "settings.h"
 
-WiFiClient          klient;
+//#define MULT		1000000L
+#define PIN_Vcc		4	//gpio4 or D2 on nodeMCU v3 to get Vcc
+#define PIN_Mode    	5       //gpio5 or D1 on nodeMCU v3 to change the Mode
 
-int			avgReads	= 5;
+WiFiClient          	klient;		//ThingSpeak client
+WiFiClient		glient;		//Google client
+//WiFiClientSecure	client;		//PushBullet client
+GoogleHomeNotifier 	ghn;
+
+
+int		avgReads	= 5;		//@
 String		myStatus	= "";
-String		warningStatus = "";
+String		warningStatus 	= "";
+String		msg		= "";
+String		notif		= "";
 
-String		note1		= "{\"type\": \"note\", \"title\": \"Moisture warning\", \"body\": \"";
-String		note2		= "\"}\r\n";
-
-int	statusCode;
-
-int	count		=	0;
-int	pw_sens[]	=	{14,12,13,15};	//pins to power the sensors
-int	m_sens[]	=	{0,0,0,0};		//the measured values from sensors
-int	m_per[]		=	{0,0,0,0};		//the percent values
-int	active		=	1000;			//1sec power on to allow stabilization
-int	idle		=	3000;			//3sec to allow discharge of output cap
-int	H_max		=	770;
-int	H_min		=	390;
+String		notif1		= "Moisture level too low for ";
+String		notif3		= " pots!";
+String      	notif5      	= "Battery level bellow minimum";   //minimum = 2.9V [?]
 
 
-//---Functions------------------------------------------------------------------
+//---------------------in settings.h-------------
+String		google		= "www.google.com";	
+const 		char displayName[] 	= "Hey Google";	        //The name of your Google Home (mini) as given in its settings
+//-----------------------------------------------
+
+        int	count		=	0;
+        int	pw_sens[]	=	{14,12,13,15};	//pins to power the sensors
+        int	m_sens[]	=	{0, 0, 0, 0 };	//the measured values from sensors
+        int	m_per[]		=	{0, 0, 0, 0 };	//the percent values
+	bool	warn[]		=	{0, 0, 0, 0 };	//the fields with warning msg
+        int	active		=	1000;		//1sec power on to allow stabilization
+        int	idle		=	3000;		//3sec to allow discharge of output cap
+	int	H_max		=	582;            //get this values from calibration
+	int	H_min		=	339;            //get this values from calibration
+
+	int	hp_minW		=	40;		//min humidity Warning level 50%
+	int	second      	= 	0;
+	int	minute      	= 	0;
+	int	hour        	= 	0;
+        
+	float   vcc     	=   0.0;
+        float   Vcc_min 	=   2.9;
+        float   V_corr  	=   1.5;    		//corection due to the measurement circuit
+        int 	d           	=   500;
+
+String  getTime();
+void    readMoist();
+
+void setup()
+{
+	Serial.begin(115200);        	// Initialize serial
+	Serial.println("\nFor calibration loop put a jumper from D1 (gpio5) to GND");
+
+	pinMode(PIN_Mode,INPUT_PULLUP);
+	while(digitalRead(PIN_Mode) == LOW)   //if PIN_Mode is LOW goes to Calibration
+	{
+		//calibration code -> see Calibration.ino
+		//not yet now
+		Serial.println("Calibration loop");
+		delay(5000);
+	}
+	WiFi.disconnect();				// 
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, pass);
+	delay(10);
+
+	while(WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}//while
+	
+	Serial.print(" Connected to IP: ");		//@
+	Serial.println(WiFi.localIP());			//@
+	ThingSpeak.begin(klient);    			// Initialize ThingSpeak
+
+	//--------------------ghn
+	Serial.println("\nConnecting to Google Home Mini...");
+	if (ghn.device(displayName, "en") != true) 
+	{
+		Serial.println(ghn.getLastError());
+		return;
+	}
+	Serial.print("found Google Home(");		//@
+	Serial.print(ghn.getIPAddress());		//@
+	Serial.print(":");				//@
+	Serial.print(ghn.getPort());			//@
+	Serial.println(")");				//@
+	//--------------------ghn
+	Serial.println("\n\rMultiSensor_2_5.ino - Setup done.");
+
+	//Vcc read:
+	pinMode(PIN_Vcc, OUTPUT);
+	digitalWrite(PIN_Vcc,HIGH); 			//enables the reading of Vcc
+	delay(d);                   			//or less
+	int Vcc = analogRead(A0);   			//Vcc to be sent to TS
+	delay(d);
+	digitalWrite(PIN_Vcc,LOW);  			//disables the reading of Vcc
+	vcc = V_corr * Vcc / 100 ;             		//Vcc is integer, vcc is float
+	Serial.print("Vcc = ");Serial.print(vcc);Serial.println(" V");
+}
+
+void loop()
+{
+//---ask Google for time--------------------------------------------------------
+	String t = getTime();
+	Serial.println(t);				//@
+//TODO check for error messages!
+
+//Read the four sensors
+	Serial.println("readMoist()");			//@
+	readMoist();        				//read the sensors and fill the values in m_per[]
+    
+//Write m_per[] to ThingSpeak
+	Serial.println("TS.setFields -->");		//@
+	ThingSpeak.setField(field1, m_per[0]);
+	ThingSpeak.setField(field2, m_per[1]);
+	ThingSpeak.setField(field3, m_per[2]);
+	ThingSpeak.setField(field4, m_per[3]);
+	ThingSpeak.setField(field5, vcc);
+
+//check for warning level 
+	int m = 0;
+	int flag = 0;
+	for (count = 0; count < 4; count++)
+	{
+		m = m_per[count];
+		if (m <= hp_minW)
+		{
+			warn[count] = 1;//True
+			flag++;
+		}//if
+	}//for
+	
+//Feed the ThingSpeak channel 
+	msg = " -> ";
+	for(int k = 0; k < 4; k++)
+	{
+		msg += "M" + String(k + 1) + "=" + String(m_per[k]) + ", ";
+	}
+	msg += "Vcc = " + String(vcc);  //Vcc is read from A0, vcc is calculated as float
+	myStatus = String(t) + msg;
+	// set the status
+	Serial.println("TS.setStatus -->");//@
+	ThingSpeak.setStatus(myStatus);
+
+	int x = ThingSpeak.writeFields(ChannelNumber, myWriteAPIKey);
+	if(x == 200)
+	{
+		Serial.println("Channel update OK.");
+	}//if
+	else
+	{
+		Serial.println("Problem updating channel. HTTP error code " + String(x));
+	}//else
+	
+	if (flag > 0)
+	{
+		//message to Tweeter, from ThingSpeak
+		int tt = ThingSpeak.setTwitterTweet(TWITTER_ACC, myStatus);
+		if(tt == 200)
+		{
+			Serial.println("Tweet sent OK");
+		}
+		else
+		{
+			Serial.println("Twitter error code " + String (tt));
+		}
+		//-------------------------------------------------------Twitter
+		
+		//message to Google Home Notifier
+		String notif2 = String(flag);   //how many are at low level
+		notif = notif1 + notif2 + notif3;
+		if ((hour >= 8) && (hour < 22))
+		{
+			if (ghn.notify(notif.c_str()) != true) 
+			{
+				Serial.println(ghn.getLastError());
+				//return;
+			}
+		}
+        
+		//------------------------------------------Google Home Notifier
+		flag = 0;
+	}
+    //message to Google Home Notifier if Vcc < Vcc_min
+    if (vcc < Vcc_min)
+    {
+        notif = notif5;
+        if ((hour >= 8) && (hour < 22))
+		{
+			if (ghn.notify(notif.c_str()) != true) 
+			{
+				Serial.println(ghn.getLastError());
+				//return;
+			}
+		}
+    }
+    
+    Serial.println("Hour: " + String(hour));
+    WiFi.disconnect();
+    Serial.println("WiFi disconnected.");
+    long sleep = SLEEP_TIME_SECONDS * 1000000L - 8;
+    Serial.print("Go to sleep for ");
+    Serial.print(SLEEP_TIME_SECONDS);
+    Serial.println(" Seconds" );//
+    ESP.deepSleep(sleep);
+
+}
+
+//----------Functions-----------------------------------------------------------
+String getTime()
+{
+//in the main prog is defined the client to access google as glient
+  Serial.println(F("connecting to google"));//@
+  //Serial.println(google);			//@
+  glient.setTimeout(5000);                  //at the beginning
+  
+  const int httpPort = 80;                  //at the beginning
+  if (!glient.connect(google, httpPort)) 
+  {
+  Serial.println(F("connection failed"));
+  return "google_fail";
+  }
+
+  // This will send the request to the server
+  glient.println("HEAD / HTTP/1.1");
+  glient.println("Host: www.google.com"); // "Host: www.google.com"
+  glient.println("Accept: */*");
+  glient.println("User-Agent: Mozilla/4.0 (compatible; esp8266 Arduino;)");
+  glient.println("Connection: close");
+  glient.println();
+  delay(500);
+
+  // Read all the characters of the reply from server and print them to Serial
+  String reply = String("");
+  while(glient.available())
+  {
+    char c = glient.read();
+    reply = reply + String(c);
+  }
+  //Serial.print(reply);		//@
+  
+  //String d = reply.substring(reply.indexOf("Date: ")+11,reply.indexOf("Date: ")+23);
+  String t = reply.substring(reply.indexOf("Date: ")+23,reply.indexOf("Date: ")+35);
+
+  hour  = t.substring(0, 2).toInt();
+  minute  = t.substring(3, 5).toInt();
+  second  = t.substring(6, 8).toInt();
+
+  hour = hour + 20;//for summer is 20, for winter is 19
+    if (hour >= 24) 
+    {
+        hour = hour % 24;
+    }
+
+  if(!glient.connected())
+  {
+    //Serial.println("disconnecting");
+    glient.stop();
+  }
+  Serial.println("connection closed");		//@
+  return t;
+}
+
 void readMoist()    //new funtion
 {
-	int an = 0;
-	int m = 0;
-	for(count = 0; count < 4; count++)
-	{//put all control pins at GND -> sensors are inactive
-		pinMode(pw_sens[count],OUTPUT);
-		digitalWrite(pw_sens[count], LOW);
-		//Serial.print(pw_sens[count]);//@
-	}//for
+    int an = 0;
+    for(count = 0; count < 4; count++)
+    {//put all control pins at GND -> sensors are inactive
+        pinMode(pw_sens[count],OUTPUT);
+        digitalWrite(pw_sens[count], LOW);
+    }//for
 
     for(count = 0; count < 4; count++)
     {
@@ -68,9 +292,7 @@ void readMoist()    //new funtion
 		//analog read for sensor 'count'
 		an = analogRead(A0);
 		m = an;
-//		digitalWrite(pw_sens[count],HIGH);
-//		Serial.println(pw_sens[count]);
-//		Serial.println("\t"+String(count)+"\t"+String(m));//@ to remove
+
 		//to prevent outliers above 100% and below 0%
 		if (an < H_min)
 		{
@@ -81,182 +303,13 @@ void readMoist()    //new funtion
 			m = H_max;
 		}
 		//put the result in m_sens[count]
-		Serial.println("\t" + String(count) + "\t" + String(an) + "\t" + String(m));	//@
+		//Serial.println("\t" + String(count) + "\t" + String(an) + "\t" + String(m));	//@
 		m_sens[count] = m;
 		//Serial.println(m);
 		m_per[count] = map(m, H_min, H_max, 100, 0);
+        Serial.println("\t" + String(count) + "\t" + String(an) + "\t" + String(m_per[count]));	//@
 		digitalWrite(pw_sens[count], LOW);
 		delay(idle);
-		//Serial.print(String(m_per[count]));
-		//Serial.println("\t" + String(count) + "\t" + String(an) + "\t" + String(m));	//@
 
     }//for
 }//readMoist() new function
-
-String setStatus()
-{
-    String status = "";
-	for(count = 0; count < 4; count++)
-	{
-		status += "\t m[" + String(count) +"] = " + String(m_per[count]);
-	}
-    return status;
-}
-
-//---End-Functions--------------------------------------------------------------
-
-
-void setup()
-{
-    Serial.begin(115200);        // Initialize serial
-    ThingSpeak.begin(klient);    // Initialize ThingSpeak
-    //WiFi.disconnect();            // Just to be sure
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, pass);
-    delay(10);
-
-    //WiFiClientSecure    client;
-
-    Serial.println("\n\rSetup done.");
-}
-
-void loop()
-{
-//Connection to WiFi
-    while(WiFi.status() != WL_CONNECTED)
-        {
-          delay(500);
-          Serial.print(".");
-        }//while
-    Serial.print(" WiFi connected to ");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-//....
-//Connection to PushBullet
-//Use WiFiClientSecure class to create TLS connection
-
-	WiFiClientSecure  client;
-    Serial.print("Connecting to: ");    //@
-    Serial.println(host);
-
-    //Serial.printf("Using fingerprint '%s'\n", fingerprint);    //@
-    client.setFingerprint(fingerprint);
-
-    if (!client.connect(host, httpsPort))
-    {
-        Serial.println("----- Connection failed");
-        return;
-    }
-
-    if (client.verify(fingerprint, host))
-    {
-        Serial.println("   Certificate matches");
-    }
-    else
-    {
-        Serial.println("   Certificate doesn't match");
-    }
-
-//Read the four sensors
-	Serial.println("readMoist()");//@
-    readMoist();        //read the sensors and fill the values in m_per[]
-
-//Write m_per[] to ThingSpeak
-	Serial.println("setFields");//@
-    ThingSpeak.setField(5, m_per[0]);
-    ThingSpeak.setField(6, m_per[1]);
-    ThingSpeak.setField(7, m_per[2]);
-    ThingSpeak.setField(8, m_per[3]);
-
-//check for warning level (m <= 50 AND m > 40)
-    int m = 0;
-    for (count = 0; count < 4; count++)
-    {
-        m = m_per[count];
-        if ((m <= 50) && (m > 40))
-        {// for m between 40 and 50 the Tweet warning goes from here
-         // for m less than 40 then ThingSpeak takes care for Tweeting it.
-		 //TODO: to send a push notification by PushBullet using ThingHTTP
-            warningStatus = "\n\rField" + String(count + 5) + ": warning level " + String(m);
-            int tt = ThingSpeak.setTwitterTweet(TWITTER_ACC, warningStatus);
-			Serial.println(warningStatus);	//@
-            if(tt == 200)
-            {
-                Serial.println("Tweet sent OK");
-            }
-            else
-            {
-                Serial.println("Twitter error code " + String (tt));
-            }
-		//note1 = "{\"type\": \"note\", \"title\": \"Moisture warning\", \"body\": \"";
-		//note2 = "\"}\r\n"		;
-			String msg = "";
-			for(count = 0; count < 4; count++)
-			{
-				msg += "\tM" +String(count + 1) + " = " + String(m_per[count]);
-			}
-			String note = note1 + String(msg) + note2;
-			Serial.println(note);        //@
-
-			//This sends a note to PushBullet.
-			//TODO: to look after the reply from PushBullet and check the error code.
-			String url = "/v2/pushes";
-			client.print(String("POST ") + url + " HTTP/1.1\r\n" +
-					"Host: " + host + "\r\n" +
-					"User-Agent: ESP8266\r\n" +
-					"Access-Token: " + accessToken + "\r\n" +
-					"Content-length: " + String(note.length()) + "\r\n"
-					"Content-Type: application/json\r\n" +
-					"Connection: close\r\n\r\n");
-			client.print(note);
-			Serial.println(note);//@
-			//here comes the code for treating the reply from PushBullet
-
-			while (client.connected())
-			{
-				String line = client.readStringUntil('\n');
-				if (line == "\r")
-				{
-					Serial.println("headers received");
-					break;
-				}
-			}//while
-			String line = client.readStringUntil('\n');
-			//	String line = client.readString(); // this line is good for debugging error messages more then a single line
-			if (line.startsWith("{\"active\":true"))
-			{
-				Serial.println("ESP8266_Solar_Moisture push successfully !");
-			}
-			else
-			{
-				Serial.println("ESP8266_Solar_Moisture push has failed");
-			}
-			Serial.println("reply was:");
-			Serial.println(line);
-			Serial.println("closing connection");
-		}//if
-	}//for
-
-    myStatus = setStatus() + warningStatus;
-    // set the status
-    ThingSpeak.setStatus(myStatus);
-	Serial.println(myStatus);//@
-	int x;
-    x = ThingSpeak.writeFields(ChannelNumber, myWriteAPIKey);
-    if(x == 200)
-    {
-        Serial.println("Channel update OK.");
-    }//if
-    else
-    {
-        Serial.println("Problem updating channel. HTTP error code " + String(x));
-    }//else
-
-    WiFi.disconnect();
-
-    long sleep = SLEEP_TIME_SECONDS * 1000000;
-	//long sleep = 10*1000000;
-    Serial.println("Go to sleep for "+ String(sleep) + " microSeconds" );//@ to remove
-    ESP.deepSleep(sleep);
-	//delay(sleep/1000);//@ to remove when use the deep sleep
-}
